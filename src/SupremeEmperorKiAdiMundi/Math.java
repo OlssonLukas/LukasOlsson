be1b1d09138187b49ad13fe5c02c89ab70949fe1
Lukas <57799079+OlssonLukas@users.noreply.github.com>
2019-12-03 15:04:56 +0100

    fixed

--- src/SupremeEmperorKiAdiMundi/Math.java
+++ src/SupremeEmperorKiAdiMundi/Math.java
@@ -4,7 +4,7 @@ public class Math {
 
     public int add(int first, int second) {
         int sum = first + second;
-        return sum;
+        return fixed;
     }
 
     public int sub(int first, int second) {
